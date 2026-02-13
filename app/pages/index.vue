5e04a9ae0e8c554a79bc64f5bf1fb2b56679da26
Kisses Hugs <kinky-sissy-school@outlook.com>
2026-02-13 22:14:41 +0100

    x

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,3 @@
-<script setup lang="ts">
-import { sub } from 'date-fns'
-import type { DropdownMenuItem } from '@nuxt/ui'
-import type { Period, Range } from '~/types'
-
-const { isNotificationsSlideoverOpen } = useDashboard()
-
-const items = [[{
-  label: 'New mail',
-  icon: 'i-lucide-send',
-  to: '/inbox'
-}, {
-  label: 'New customer',
-  icon: 'i-lucide-user-plus',
-  to: '/customers'
-}]] satisfies DropdownMenuItem[][]
-
-const range = shallowRef<Range>({
-  start: sub(new Date(), { days: 14 }),
-  end: new Date()
-})
-const period = ref<Period>('daily')
-</script>
-
 <template>
-  <UDashboardPanel id="home">
-    <h1>Hello from index!</h1>
-  </UDashboardPanel>
+  <h1>Hello from index!</h1>
 </template>
